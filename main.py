651f0ae9469da3cd46fd46e1e45b0a0446671329
Lucas <lucasdastre10@gmail.com>
2024-08-05 17:42:18 -0300

    salvando meu arquivo novamente

---
+++ main.py
@@ -0,0 +1,16 @@
+print("jornada de dados")
+
+#variaveis
+
+idade = 34
+nome = "Lucas"
+
+print(idade)
+
+nova_idade = idade + 10
+print(nova_idade)
+
+lista = [1,2,3,4,5]
+
+
+lista_invest = lista.reverse
